ed9bc5127480ea824b9bc724b2b1e33d9b63f480
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2021-03-18 15:20:36 -0400

    Setup math for PID

---
+++ include/pidcontroller.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/* 
+ * Stores PID constants, such as Proportional, Integral, Derivative weights 
+*/
+typedef struct PIDInfo {
+    double p;
+    double i;
+    double d;
+
+    PIDInfo(double p, double i, double d);
+    PIDInfo();
+} PIDInfo;
+
+class PIDController {
+    private:
+        double current;
+        double error, lastError;
+        double integral, derivative;
+        double target;
+        double speed;
+
+        double settleStart;
+        bool settling, settled;
+
+        double tolerance;
+        PIDInfo constants;
+    public:
+        PIDController(double target, double tolerance, PIDInfo constants);
+        
+        double step(double sensorVal); // Returns speed
+
+        void setTarget(double newTarget); // Set new target
+        void reset(); // Reset all values
+
+        bool isSettled();
+};
---
+++ src/pidcontroller.cpp
@@ -0,0 +1,73 @@
+#include "pidcontroller.h"
+#include "globals.h"
+#include "main.h"
+
+// Unit is milliseconds
+#define SETTLE_DELAY 200 
+
+// Initialize with defaults
+PIDInfo::PIDInfo() {
+    this->p = 1;
+    this->i = 1;
+    this->d = 1;
+}
+
+// Initialize with params
+PIDInfo::PIDInfo(double p, double i, double d) {
+    this->p = p;
+    this->i = i;
+    this->d = d;
+}
+
+PIDController::PIDController(double target, double tolerance, PIDInfo constants) {
+    this->target = target;
+    this->tolerance = tolerance;
+    this->constants = constants;
+}
+
+double PIDController::step(double sensorVal) {
+    // Set current to sensorVal
+    this->current = sensorVal;
+
+    // Get new error
+    this->error = this->target - this->current;
+
+    // Add error to integral since its the sum of all errors
+    this->integral += this->error;
+
+    // Get the derivative by getting rate of change
+    this->derivative = this->error - this->lastError;
+
+    // Disable integrals until it comes into usable range
+    if (this->error == 0) this->integral = 0;
+    if (abs(this->error) > (127 / 2)) this->integral = 0;
+
+    // Run the calculation
+    this->speed = (this->constants.p * this->error) + (this->constants.i * this->integral) + (this->constants.d * this->derivative);
+
+    // Robot likely stopped if two previous errors were under tolerance
+    if (abs(this->error) <= this->tolerance && abs(this->lastError) <= this->tolerance) { 
+        // Stop for a specific amount of time before being completely settled, should ensure that we are in exact position we want to be in
+        if (!this->settling) {
+            this->settleStart = pros::millis();
+            this->settling = true;
+        }
+
+        // We shouldn't be moving at all until the settle time stops
+        this->speed = 0;
+        
+        if (pros::millis() - this->settleStart > SETTLE_DELAY) {
+            // Waited long enough, we can finally settle
+            this->settled = true;
+        }
+    } else {
+        // Don't settle
+        this->settling = false;
+        this->settled = false;
+    }
+
+    // Set last error to current error for next iter
+    this->lastError = this->error;
+
+    return this->speed;
+}
